0f4d7b1e15a81557003d5bb011615a919116ca03
Cacao1522 <calstone841@gmail.com>
2024-05-14 16:07:54 +0900

    print

--- kadai/src/search/Search.java
+++ kadai/src/search/Search.java
@@ -20,8 +20,14 @@ public class Search {
 
         System.out.print("検索する言葉を入力してください：");
         String word = scan.next(); // 検索word
-        DBManager.getInstance().getTwit(word); // DB操作
-
+        List<Twit> tl_list = DBManager.getInstance().getTwit(word); // DB操作
+        // Twitsを表示
+        for (Twit tw : tl_list) {
+            System.out.println("------------------------------------------------------------------");
+            System.out.println(tw.id + ". " + tw.name);
+            System.out.println(tw.createdAt);
+            System.out.println(tw.content);
+        }
         scan.close();
     }
 }

5644afac8ea9f1180421cea0243b3efac621f397
gtzx1584 <gangtianzhixi486@gmail.com>
2024-05-17 18:26:49 +0900

    MethodOrder

--- kadai/src/TwitTest.java
+++ kadai/src/TwitTest.java
@@ -2,6 +2,7 @@ import java.sql.*;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
 
 import dbmanager.DBManager;
@@ -15,29 +16,29 @@ import post.Post;
 
 @FixMethodOrder (MethodSorters.NAME_ASCENDING)
 public class TwitTest {
-    @Test public void test01_signupTest(){
-        Signup signup = Signup.getInstance();
-        for (int i = 0; i < 5; i++) {
-            signup.signup();
-            System.out.println();
-        }
+    // @Test public void test01_signupTest(){
+    //     Signup signup = Signup.getInstance();
+    //     for (int i = 0; i < 5; i++) {
+    //         signup.signup();
+    //         System.out.println();
+    //     }
 
-        Connection con = null;
-        try {
-            Class.forName("org.sqlite.JDBC");
-            con = DriverManager.getConnection("jdbc:sqlite:../db/twitDB.db");
-            PreparedStatement pstmt = con.prepareStatement("select * from account");
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()) {
-                System.out.println(rs.getString("id") + ":" + rs.getString("name") + ":" + rs.getString("password"));
-            }
-            rs.close();
-            pstmt.close();
-            con.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    //     Connection con = null;
+    //     try {
+    //         Class.forName("org.sqlite.JDBC");
+    //         con = DriverManager.getConnection("jdbc:sqlite:../db/twitDB.db");
+    //         PreparedStatement pstmt = con.prepareStatement("select * from account");
+    //         ResultSet rs = pstmt.executeQuery();
+    //         while (rs.next()) {
+    //             System.out.println(rs.getString("id") + ":" + rs.getString("name") + ":" + rs.getString("password"));
+    //         }
+    //         rs.close();
+    //         pstmt.close();
+    //         con.close();
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //     }
+    // }
 
     @Test public void test02_loginTest(){
         Login login = Login.getInstance();

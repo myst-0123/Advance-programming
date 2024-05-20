17a4b9e7b6ce42512f0d16112b378ac94628cde0
Hida00 <shoki.hida@gmail.com>
2024-05-21 01:11:40 +0900

    fix: TimeLine CL

--- kadai/src/timeline/ShowTimeLineCL.java
+++ kadai/src/timeline/ShowTimeLineCL.java
@@ -12,25 +12,10 @@ public class ShowTimeLineCL {
     private Timer timer;
     private TimerTask task;
 
-    private int TL_UPDATE_TIME = 5;
-
     // コンストラクタ, TLの最大表示数と更新頻度を設定
-    public ShowTimeLineCL(int max_twits, int update_time) {
-        TL_UPDATE_TIME = update_time;
+    public ShowTimeLineCL(int max_twits) {
         // TimeLineクラスのインスタンスを生成
         tl = new TimeLine(max_twits);
-
-        // 定期実行を行うためのTimer, TimerTask
-        // timer = new Timer(false);
-        // task = new TimerTask() {
-        // @override
-        // public void run() {
-        // ShowTimeLine();
-        // }
-        // };
-        // TL_UPDATE_TIME毎にShowTimeLineを実行
-        // 単位がmsのため、1000倍する
-        // timer.schedule(task, 0, TL_UPDATE_TIME * 1000);
     }
 
     // TimeLineの表示
@@ -50,34 +35,5 @@ public class ShowTimeLineCL {
             System.out.println(tw.content);
         }
         System.out.println("------------------------------------------------------------------");
-    }
-
-    // TimeLineの更新の停止
-    public void StopUpdateTL() {
-        if (task == null) {
-            System.out.println("停止中");
-        }
-        // Taskのキャンセル
-        task.cancel();
-        // 停止している判別が出来るようにTaskをnullにする
-        task = null;
-        System.out.println("停止");
-    }
-
-    // TimeLineの更新の再開
-    public void RestartUpdaeteTL() {
-        if (task != null) {
-            System.out.println("更新中");
-        }
-        // Taskインスタンスの再生成
-        // task = new TimerTask() {
-        // @override
-        // public void run() {
-        // ShowTimeLine();
-        // }
-        // };
-        // TL_UPDATE_TIME毎にShowTimeLineを実行
-        timer.schedule(task, 0, TL_UPDATE_TIME * 1000);
-        System.out.println("再開");
     }
 }

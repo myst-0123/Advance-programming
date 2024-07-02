41180821a86560b8a92eb46416f5d5dad81619b5
Cacao1522 <calstone841@gmail.com>
2024-07-02 13:09:37 +0900

    a

--- Othello/myplayer/MyPlayer.java
+++ Othello/myplayer/MyPlayer.java
@@ -6,6 +6,7 @@ import static ap24.Color.BLACK;
 import static ap24.Color.WHITE;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.IntStream;
 import ap24.Board;
@@ -14,39 +15,43 @@ import ap24.Move;
 
 class MyEval {
     static float[][] M = {
-        { 10, 10, 10, 10, 10, 10 },
-        { 10, -5,  1,  1, -5, 10 },
-        { 10,  1,  1,  1,  1, 10 },
-        { 10,  1,  1,  1,  1, 10 },
-        { 10, -5,  1,  1, -5, 10 },
-        { 10, 10, 10, 10, 10, 10 },
+            { 10, 10, 10, 10, 10, 10 },
+            { 10, -5, 1, 1, -5, 10 },
+            { 10, 1, 1, 1, 1, 10 },
+            { 10, 1, 1, 1, 1, 10 },
+            { 10, -5, 1, 1, -5, 10 },
+            { 10, 10, 10, 10, 10, 10 },
     };
 
     // boardのスコアを取得する
     public float value(Board board) {
-        if (board.isEnd()) return 1000000 * board.score();
+        if (board.isEnd())
+            return 1000000 * board.score();
 
         return (float) IntStream.range(0, LENGTH)
-          .mapToDouble(k -> score(board, k))
-          .reduce(Double::sum).orElse(0);
+                .mapToDouble(k -> score(board, k))
+                .reduce(Double::sum).orElse(0);
     }
 
     // 重み付きでスコアを返す
     float score(Board board, int k) { // 課題2e 参考:https://bassy84.net/#google_vignette
-        int l_b = ((MyBoard)board).findLegalIndexes(BLACK).size(); //BLACKの合法手
-        int l_w = ((MyBoard)board).findLegalIndexes(WHITE).size(); //WHITEの合法手
-        int n_b = ((MyBoard)board).count(BLACK); //BLACKの石の数
-        int n_w = ((MyBoard)board).count(WHITE); //WHITEの石の数
+        int l_b = ((MyBoard) board).findLegalIndexes(BLACK).size(); // BLACKの合法手
+        int l_w = ((MyBoard) board).findLegalIndexes(WHITE).size(); // WHITEの合法手
+        int n_b = ((MyBoard) board).count(BLACK); // BLACKの石の数
+        int n_w = ((MyBoard) board).count(WHITE); // WHITEの石の数
 
-        int[] w = new int[5]; //w_1~w_5
-        if(n_b + n_w <= 12) w = new int[] {1, +30, -60, +2, -1}; //序盤:石を多く取らないようにする≒合法手の数を重視する
-        else if(n_b + n_w <= 24) w = new int[] {3, +30, -60, +5, -5}; //中盤:合法手の数を重視しつつ、隅や辺にも注意する 石の数にも気をつける
-        else w = new int[] {10, +20, -40, +10, -20}; //終盤:隅、辺と石の数に注目する
+        int[] w = new int[5]; // w_1~w_5
+        if (n_b + n_w <= 12)
+            w = new int[] { 1, +30, -60, +2, -1 }; // 序盤:石を多く取らないようにする≒合法手の数を重視する
+        else if (n_b + n_w <= 24)
+            w = new int[] { 3, +30, -60, +5, -5 }; // 中盤:合法手の数を重視しつつ、隅や辺にも注意する 石の数にも気をつける
+        else
+            w = new int[] { 10, +20, -40, +10, -20 }; // 終盤:隅、辺と石の数に注目する
         return w[0] * M[k / SIZE][k % SIZE] * board.get(k).getValue()
-               + w[1] * l_b
-               + w[2] * l_w
-               + w[3] * n_b
-               + w[4] * n_w;
+                + w[1] * l_b
+                + w[2] * l_w
+                + w[3] * n_b
+                + w[4] * n_w;
     }
 }
 
@@ -80,7 +85,9 @@ public class MyPlayer extends ap24.Player {
         }
     }
 
-    boolean isBlack() { return getColor() == BLACK; }
+    boolean isBlack() {
+        return getColor() == BLACK;
+    }
 
     // 決定した行動を返す
     public Move think(Board board) {
@@ -103,7 +110,8 @@ public class MyPlayer extends ap24.Player {
 
     // maxサーチ
     float maxSearch(Board board, float alpha, float beta, int depth) {
-        if (isTerminal(board, depth)) return this.eval.value(board);
+        if (isTerminal(board, depth))
+            return this.eval.value(board);
 
         var moves = board.findLegalMoves(BLACK);
         moves = order(moves);
@@ -130,16 +138,18 @@ public class MyPlayer extends ap24.Player {
 
     // Minサーチ
     float minSearch(Board board, float alpha, float beta, int depth) {
-        if (isTerminal(board, depth)) return this.eval.value(board);
+        if (isTerminal(board, depth))
+            return this.eval.value(board);
 
         var moves = board.findLegalMoves(WHITE);
-        moves = order(moves);
+        moves = order(moves, board);
 
         for (var move : moves) {
             var newBoard = board.placed(move);
-            float v = maxSearch(newBoard, alpha, beta, depth+1);
+            float v = maxSearch(newBoard, alpha, beta, depth + 1);
             beta = Math.min(beta, v);
-            if (alpha >= beta) break;
+            if (alpha >= beta)
+                break;
         }
 
         return beta;
@@ -151,9 +161,16 @@ public class MyPlayer extends ap24.Player {
     }
 
     // Listのシャッフル
-    List<Move> order(List<Move> moves) {
+    List<Move> order(List<Move> moves, Board board) {
         var shuffled = new ArrayList<Move>(moves);
         Collections.shuffle(shuffled);
+
+        shuffled.sort(Comparator.comparing(move -> {
+            var newBoard = board.placed((Move) move);
+            return this.eval.value(newBoard);
+        }).reversed());
+        // nodeCount += list.size();
+
         return shuffled;
     }
 }

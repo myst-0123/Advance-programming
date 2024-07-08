95895457165f078ef862d930b170052c636fed3a
gtzx1584 <gangtianzhixi486@gmail.com>
2024-07-08 21:06:57 +0900

    score_lightの係数調整

--- Othello/g24x00/OurPlayerV2.java
+++ Othello/g24x00/OurPlayerV2.java
@@ -81,9 +81,9 @@ class MyEval2 {
     // int n_w = ((MyBoard) board).count(WHITE); // WHITEの石の数
     int[] w = new int[3]; // w_1~w_5
     if (n_b + n_w <= 12)
-      w = new int[] { 1, -1, 1 }; // 序盤:石を多く取らないようにする≒合法手の数を重視する
+      w = new int[] { 1, -20, 10 }; // 序盤:石を多く取らないようにする≒合法手の数を重視する
     else if (n_b + n_w <= 24)
-      w = new int[] { 3, 0, 0 }; // 中盤:合法手の数を重視しつつ、隅や辺にも注意する 石の数にも気をつける
+      w = new int[] { 3, 1, -2 }; // 中盤:合法手の数を重視しつつ、隅や辺にも注意する 石の数にも気をつける
     else
       w = new int[] { 10, +10, -20 }; // 終盤:隅、辺と石の数に注目する
     return w[0] * M[k / SIZE][k % SIZE] * board.get(k).getValue()
@@ -101,7 +101,7 @@ public class OurPlayerV2 extends ap24.Player {
   OurBoard board;
 
   public OurPlayerV2(Color color) {
-    this(MY_NAME, color, new MyEval2(), 9);
+    this(MY_NAME, color, new MyEval2(), 10);
   }
 
   public OurPlayerV2(String name, Color color, MyEval2 eval, int depthLimit) {
@@ -145,7 +145,7 @@ public class OurPlayerV2 extends ap24.Player {
 
   float maxSearch(Board board, float alpha, float beta, int depth) {
     if (isTerminal(board, depth))
-      return this.eval.value(board);
+      return this.eval.value2(board);
 
     var moves = board.findLegalMoves(BLACK);
     if (depth >= 2)
@@ -173,7 +173,7 @@ public class OurPlayerV2 extends ap24.Player {
 
   float minSearch(Board board, float alpha, float beta, int depth) {
     if (isTerminal(board, depth))
-      return this.eval.value(board);
+      return this.eval.value2(board);
 
     var moves = board.findLegalMoves(WHITE);
     if (depth >= 2)
